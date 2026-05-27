ee42d266eaac09416e9ef23c6dfabf2bb4eae1cd
Vishal Makaram <visha@vishalmaks-MacBook-Pro.local>
2026-05-26 22:40:13 -0700

    test

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Grab the secret keys from your .env.local vault
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-// Build and export the connection "client"
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
